afd1445625dedd3f62f2f97a2a084d9e44b7410e
casus <casus@SALIHPC>
2023-06-23 17:38:24 +0300

    Proje dosyası ekle.

---
+++ ELKExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELKExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost("Test")]
+        public IActionResult Test()
+        {
+            return Ok("Test");
+        }
+    }
+}
---
+++ ELKExample/Program.cs
@@ -0,0 +1,30 @@
+using Elastic.Apm.NetCoreAll;
+using ELKExample.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+LoggingExtension.ConfigureLogging();
+var app = builder.Build();
+
+app.UseAllElasticApm(builder.Configuration);
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
